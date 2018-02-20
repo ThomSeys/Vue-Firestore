29a6cdf2bcbefaae3912349284363272a0e61cc1
ThomSeys <thomasseyssens82@hotmail.com>
2018-02-20 16:49:48 +0100

    adjustments

--- src/main.js
+++ src/main.js
@@ -9,16 +9,12 @@ import App from "./App";
 import store from "./store";
 import router from "./router";
 import { sync } from "vuex-router-sync";
-import MuseUI from "muse-ui";
-import "muse-ui/dist/muse-ui.css";
 import "./initFirebase";
 
 // Enable progressive web app support (with offline-plugin)
 if (process.env.NODE_ENV === "production") {
   require("./pwa");
 }
-
-Vue.use(MuseUI);
 
 Vue.config.productionTip = false;
 
